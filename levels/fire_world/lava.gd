1b065596e3cec24108b134f015c3f9ec45973050
Haunted Bees <fench@hauntedbees.com>
2026-07-13 22:11:37 -0700

    fun with lava

---
+++ levels/fire_world/lava.gd
@@ -0,0 +1,30 @@
+class_name Lava extends Area3D
+
+const _SMOKE_SCENE := preload("uid://bce761tpdiksy")
+const _FREEZE_SCENE := preload("uid://ewoeuy06x4v4")
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	area_entered.connect(_on_area_entered)
+
+func _on_body_entered(body: Node3D) -> void:
+	print("LAVA BODY %s" % body.name)
+
+func _on_area_entered(area: Area3D) -> void:
+	var parent: Node3D = area.get_parent()
+	if parent == null:
+		return
+	if parent is Projectile:
+		var effect := (parent as Projectile).effect
+		if effect == BWEnum.Effect.Burn:
+			_add_smoke(parent, false)
+		elif effect == BWEnum.Effect.Freeze:
+			var freeze_block: Node3D = _FREEZE_SCENE.instantiate()
+			add_child(freeze_block)
+			freeze_block.global_position = parent.global_position
+
+func _add_smoke(body: Node3D, extreme: bool) -> void:
+	var smoke: SmokeCloud = _SMOKE_SCENE.instantiate()
+	smoke.extreme = extreme
+	body.add_child(smoke)
+	smoke.global_position = body.global_position
--- spells/attacks/fireball.gd
+++ spells/attacks/fireball.gd
@@ -5,7 +5,6 @@ const _SMOKE_SCENE := preload("uid://bce761tpdiksy")
 @onready var _shape_cast: ShapeCast3D = %ShapeCast3D
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	_shape_cast.force_shapecast_update()
 	if body.is_in_group(&"snow"):
 		_add_smoke(body, false)
 	elif body.is_in_group(&"ice"):
@@ -16,6 +15,7 @@ func _add_smoke(body: Node3D, extreme: bool) -> void:
 	var smoke: SmokeCloud = _SMOKE_SCENE.instantiate()
 	smoke.extreme = extreme
 	body.add_child(smoke)
+	_shape_cast.force_shapecast_update()
 	if _shape_cast.is_colliding():
 		smoke.global_position = _shape_cast.get_collision_point(0)
 	else:
