81a6a83e09e5f52c7c1711866bb1264e37fa9071
Haunted Bees <fench@hauntedbees.com>
2026-07-20 19:14:35 -0700

    closes #16

--- resource_types/items/spell_book.gd
+++ resource_types/items/spell_book.gd
@@ -4,3 +4,17 @@ class_name Spellbook extends Item
 
 func _init() -> void:
 	first_get_text = "Finally! A spellbook! These portals mess with me memory, so this should help me remember a spell or two!"
+
+func can_be_combined(_me: InventoryDetail, them: InventoryDetail) -> bool:
+	return them.item is Spellbook
+
+func combine(me: InventoryDetail, them: InventoryDetail) -> void:
+	if !can_be_combined(me, them): # ONE MORE FOR GOOD MEASURE
+		return
+	var new_book: Spellbook = me.item.duplicate()
+	new_book.name = "Merged Spellbook"
+	new_book.description = "I combined multiple spellbooks to create this one."
+	for s in (them.item as Spellbook).spells:
+		if !new_book.spells.has(s):
+			new_book.spells.append(s)
+	me.item = new_book
