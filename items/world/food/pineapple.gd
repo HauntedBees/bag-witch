2519e1b7269315bf6e0b8fe2e2867c34c824382a
Haunted Bees <fench@hauntedbees.com>
2026-07-28 23:52:50 -0700

    closes #75

--- items/world/food/pineapple.gd
+++ items/world/food/pineapple.gd
@@ -1,6 +1,18 @@
 extends WorldItem
 
+var _RINGS := preload("uid://bhc3lbevd1b0w")
+
 @onready var _base: Node3D = %pineapple_base
 
 func _ready() -> void:
 	_base.visible = !from_inventory
+	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(area: Area3D) -> void:
+	if area is SwipeArea3D:
+		var p: PlayerAttack = area.swipe_attack
+		if p.weapon.is_saw:
+			var wi := _RINGS.get_world_item()
+			get_parent().add_child(wi)
+			wi.global_position = global_position
+			queue_free()
---
+++ spells/attacks/swipe_area_3d.gd
@@ -0,0 +1,3 @@
+class_name SwipeArea3D extends Area3D
+
+@export var swipe_attack: PlayerAttack
